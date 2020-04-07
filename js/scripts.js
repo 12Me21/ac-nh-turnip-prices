f135565340895c06222eb8b81d7f13745b1ca12b
12Me21 <12Me21.mc@gmail.com>
2020-04-06 21:02:47 -0400

    remove all external files, simplify page, etc.)

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,37 @@
-$(document).ready(function () {
-  // load sell_prices from local storage
-  try {
-    const sell_prices = JSON.parse(localStorage.getItem("sell_prices"));
+//don't need to use localstorage because browser autofill does that for us :)
+update();
 
-    if (!Array.isArray(sell_prices) || sell_prices.length !== 14) {
-      return;
-    }
+function update() {
+	console.log("E");
+	// Update output on any input change
+	var buy_price = parseInt(document.getElementById("buy").value);
 
-    sell_prices.forEach((sell_price, index) => {
-      if (!sell_price) {
-        return;
-      }
-
-      const buyInput = $("#buy");
-      if (index === 0) {
-        buyInput.focus();
-        buyInput.val(sell_price);
-        buyInput.blur();
-        return;
-      }
-
-      const element = $("#sell_" + index);
-      if (element.length) {
-        element.focus();
-        element.val(sell_price);
-        element.blur();
-      }
-    });
-
-    $(document).trigger("input");
-  } catch (e) {
-    console.error(e);
-  }
-
-  $("#reset").on("click", function() {
-    $("input").val(null).trigger("input");
-  })
-});
-
-$(document).on("input", function() {
-  // Update output on any input change
-
-  var buy_price = parseInt($("#buy").val());
-
-  var sell_prices = [buy_price, buy_price];
-  for (var i = 2; i < 14; i++) {
-    sell_prices.push(parseInt($("#sell_" + i).val()));
-  }
-
-  localStorage.setItem("sell_prices", JSON.stringify(sell_prices));
-
-  const is_empty = sell_prices.every(sell_price => !sell_price);
-  if (is_empty) {
-    $("#output").html("");
-    return;
-  }
-
-  let output_possibilities = "";
-  for (let poss of analyze_possibilities(sell_prices)) {
-    var out_line = "<tr><td>" + poss.pattern_description + "</td>"
-    for (let day of poss.prices.slice(1)) {
-      if (day.min !== day.max) {
-        out_line += `<td>${day.min}..${day.max}</td>`;
-      } else {
-        out_line += `<td class="one">${day.min}</td>`;
-      }
-    }
-    out_line += `<td class="one">${poss.weekMin}</td><td class="one">${poss.weekMax}</td></tr>`;
-    output_possibilities += out_line
-  }
-
-  $("#output").html(output_possibilities)
-});
+	var sell_prices = [buy_price, buy_price];
+	for (var i = 2; i < 14; i++) {
+		sell_prices.push(parseInt(document.getElementById("sell_"+i).value));
+	}
+	
+	localStorage.setItem("sell_prices", JSON.stringify(sell_prices));
+	
+	const is_empty = sell_prices.every(sell_price => !sell_price);
+	if (is_empty) {
+		document.getElementById("output").innerHTML = "";
+		return;
+	}
+	
+	var output_possibilities = "";
+	for (var poss of analyze_possibilities(sell_prices)) {
+		var out_line = "<tr><td>" + poss.pattern_description + "</td>"
+		for (var day of poss.prices.slice(1)) {
+			if (day.min !== day.max) {
+				out_line += "<td>"+day.min+".."+day.max+"</td>";
+			} else {
+				out_line += "<td class='one'>"+day.min+"</td>";
+			}
+		}
+		out_line += "<td class='one'>"+poss.weekMin+"</td><td class='one'>"+poss.weekMax+"</td></tr>";
+		output_possibilities += out_line
+	}
+	
+	document.getElementById("output").innerHTML = output_possibilities;
+}
